c60382c1353bb8e914b800e54aee2ada4acdfbc7
BjornElling <bj.elling@gmail.com>
2025-11-15 00:12:08 +0100

    favicon

--- src/config/version.js
+++ src/config/version.js
@@ -7,5 +7,5 @@
  * Redigér IKKE denne fil manuelt!
  */
 
-export const VERSION = '2025.11.7';
-export const BUILD_DATE = '2025-11-14T22:58:48.401Z';
+export const VERSION = '2025.11.8';
+export const BUILD_DATE = '2025-11-14T23:12:09.104Z';
