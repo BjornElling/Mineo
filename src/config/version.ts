6e2e4a93dc1a8444351a2dd25198c55b58d4b9c5
Bjørn Elling <bj.elling@gmail.com>
2026-03-05 10:47:42 +0100

    kapitaliseringsbekendtgørelser lagt ind

--- src/config/version.ts
+++ src/config/version.ts
@@ -5,8 +5,8 @@
  * Format: YYYY.MM.BUILD
  */
 
-export const VERSION = '2026.03.258';
-export const BUILD_DATE = '2026-03-04T21:52:36.824Z';
+export const VERSION = '2026.03.259';
+export const BUILD_DATE = '2026-03-05T09:47:44.683Z';
 
 /**
  * Filformat version - ændres kun hvis datastrukturen ændres.
