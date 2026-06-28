77ccd37c56868f2aa78230d8deb117a5607ce1c8
BjornElling <bj.elling@gmail.com>
2026-06-28 21:16:57 +0200

    tilføjet flere brugere

--- src/auth/authConfig.ts
+++ src/auth/authConfig.ts
@@ -36,6 +36,18 @@ export const SHARED_PASSWORD_HASHES = defineSharedPasswordHashes([
     hash: '324ae39817dc46525ab92dde7a1263e27b218aa14a9eb64cde10948c0a71869e',
   },
   {
+    description: 'Adam C. Kraft (Advofair)',
+    hash: 'c39e5ec0a223749925cdff2d77b07c25d7c945dbc1e3f73885599ee953a89980',
+  },
+  {
+    description: 'Lind advokatfirma',
+    hash: '8b65db82546d96350a603f094b95badcb2c31527a72751b7ea8396a302d8b38c',
+  },
+  {
+    description: 'Elmer Advokater',
+    hash: '82ca3da16dfa045fd06866b62a34b81a13ce62f37de91ceab73956bb9b4fc358',
+  },
+  {
     description: 'KSS personligt password (OBS: kun til mineo, ikke minParadigmesamling eller minDomssamling)',
     hash: '7edb340c0afc7e8d87ddc2dc75266b2f2da603d53d404da4824de6470ef9a912',
   },
