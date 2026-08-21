a5806c1bc2973dea9051a6ea6dfb485dfc5fd562
BjornElling <bj.elling@gmail.com>
2026-08-21 07:47:40 +0200

    Ret varigeMenPolicy-testen til den nye 1-4%-advarselsgrænse
    Testen forventede stadig den forladte regel (advarsel kun ved præcis 5%),
    men brugerbliksrettelsen i b90c0092 flyttede advarslen til 1-4% og hævede
    nedre bounds til 5. Testen fulgte ikke med og fejlede i GitHub Actions.

--- src/__tests__/domain/varigemen/varigeMenPolicy.test.ts
+++ src/__tests__/domain/varigemen/varigeMenPolicy.test.ts
@@ -4,14 +4,17 @@ import {
 } from '../../../domain/varigemen/varigeMenPolicy';
 
 describe('resolveVarigeMenWarning', () => {
-  it('viser den aftalte ikke-blokerende advarsel ved præcis 5 procent', () => {
-    expect(resolveVarigeMenWarning(5)).toEqual({
-      severity: 'warning',
-      message: VARIGE_MEN_FEM_PROCENT_WARNING,
-    });
-  });
+  it.each([1, 2, 3, 4])(
+    'viser den aftalte ikke-blokerende advarsel ved %s procent',
+    (value) => {
+      expect(resolveVarigeMenWarning(value)).toEqual({
+        severity: 'warning',
+        message: VARIGE_MEN_FEM_PROCENT_WARNING,
+      });
+    },
+  );
 
-  it.each([undefined, 4, 6])('viser ingen advarsel ved %s', (value) => {
+  it.each([undefined, 0, 5, 6])('viser ingen advarsel ved %s', (value) => {
     expect(resolveVarigeMenWarning(value)).toBeUndefined();
   });
 });
